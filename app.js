e74c5d8388437863aa7f9ba43924ad61154df86a
liuqiqi <1239245052@qq.com>
2017-05-08 22:09:55 +0800

    添加注释

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var birds = require('./routes/birds');
 
 var app = express();
 
 // view engine setup
+// 指定视图所在的位置
 app.set('views', path.join(__dirname, 'views'));
+// 注册模板引擎
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -20,10 +23,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 通过 Express 内置的 express.static 可以方便地托管静态文件，例如图片、CSS、JavaScript 文件等。
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/birds', birds);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +41,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+// 在其所有他中间件的后面添加一个处理 404 的中间件
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
